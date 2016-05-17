127f3bc532139136af5bf8ca6c10608bc52afcc5
Noblind <noblind@Mac-mini-de-Noblind.local>
2016-05-17 18:59:27 -0300

    Alterado app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Clean Blog</title>
+    <title>Blog de Notícias</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
